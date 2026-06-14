94e3be4a609f8b13e98a9a82e9ae18321e8afe5a
crux-ci <ci@crux360.ai>
2026-06-14 18:03:56 +0000

    release: CruxSDK v1.0.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CruxSDK",
-            url: "https://github.com/crux-platform/crux-ios-sdk-release/releases/download/v1.0.2/CruxSDK-1.0.2.xcframework.zip",
-            checksum: "a3d5dae7e0787dd4739d0414ac1b97152ba0e5434b48fc88eca207ab7d957bb2"
+            url: "https://github.com/crux-platform/crux-ios-sdk-release/releases/download/v1.0.3/CruxSDK-1.0.3.xcframework.zip",
+            checksum: "a5cbb8ed1ddfd21509ff26fa68e5875b68a33a4f4e4b5ca4cc1fe516ef7acabe"
         )
     ]
 )
